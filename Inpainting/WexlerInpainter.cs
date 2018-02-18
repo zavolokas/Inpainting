5f01d24910062ea60280ee81aa732275b1da4327
zavolokas <sergey.zavoloka@gmail.com>
2018-02-18 07:48:03 +0100

    Gether some stats from inpainter
    to be able to decide what to do next

--- Inpainting/WexlerInpainter.cs
+++ Inpainting/WexlerInpainter.cs
@@ -1,12 +1,12 @@
-
 using Zavolokas.Structures;
 
 namespace Zavolokas.ImageProcessing.Inpainting
 {
     public static class WexlerInpainter
     {
-        // TODO: optimize & refactor WexlerInpainter
-        public static void Inpaint(ZsImage image, Area2D removeArea, Nnf nnf, int patchSize, ColorResolver colorResolver, double k)
+        private static Result _result;
+
+        public static Result Inpaint(ZsImage image, Area2D removeArea, Nnf nnf, int patchSize, ColorResolver colorResolver, double k)
         {
             // Since the nnf was normalized, the sigma now is normalized as well and it
             // has not any more some specific value.
@@ -14,6 +14,9 @@ namespace Zavolokas.ImageProcessing.Inpainting
             //const double minusSigmaCube2 = -0.91125;//-2 * sigma * sigma; sigma = 0.675 = 75percentil
             //double NaturalLogBase2 = System.Math.Pow(System.Math.E, 1.0 / minusSigmaCube2);
             const double naturalLogBasePowMinusSigmaCube2 = 0.33373978049163078;
+
+            const double pixelChangeTreshold = 0.00003;
+            const double maxSquareDistanceInLab = 32668.1151;
 
             // Get points' indexes that that needs to be inpainted.
             var pointIndexes = new int[removeArea.ElementsCount];
@@ -33,6 +36,11 @@ namespace Zavolokas.ImageProcessing.Inpainting
             // Weighted color is color's components values + weight of the color.
             var weightedColors = new double[patchSize * patchSize * (cmpts + 1)];
             var confidence = removeArea.CalculatePointsConfidence(1.5, 1.3);
+            var resolvedColor = new double[cmpts];
+
+            var totalDifference = 0.0;
+            var changedPixelsAmount = 0;
+            var changedPixelsDifference = 0.0;
 
             for (int ii = 0; ii < pointIndexes.Length; ii++)
             {
@@ -77,8 +85,44 @@ namespace Zavolokas.ImageProcessing.Inpainting
                     }
                 }
                 // calculate color
-                colorResolver.Resolve(weightedColors, 0, colorsCount, cmpts, k, image.PixelsData, pointIndex);
+                colorResolver.Resolve(weightedColors, 0, colorsCount, cmpts, k, resolvedColor, 0);
+
+                // how different is resolvedColor from the old color?
+                var labL = resolvedColor[0] - image.PixelsData[pointIndex * cmpts + 0];
+                var labA = resolvedColor[1] - image.PixelsData[pointIndex * cmpts + 1];
+                var labB = resolvedColor[2] - image.PixelsData[pointIndex * cmpts + 2];
+                var pixelsDiff = (labL*labL + labA*labA + labB*labB) / maxSquareDistanceInLab;
+                totalDifference += pixelsDiff;
+
+                if (pixelsDiff > pixelChangeTreshold)
+                {
+                    changedPixelsAmount ++;
+                    changedPixelsDifference += pixelsDiff;
+                }
+
+                for (var i = 0; i < cmpts; i++)
+                {
+                    image.PixelsData[pointIndex * cmpts + i] = resolvedColor[i];
+                }
             }
+
+            totalDifference /= pointIndexes.Length;
+            _result.TotalDifference = totalDifference / pointIndexes.Length;
+            _result.PixelsChangedAmount = changedPixelsAmount;
+            _result.PixelsToInpaintAmount = pointIndexes.Length;
+            _result.ChangedPixelsDifference = changedPixelsDifference;
+
+            return _result;
+        }
+
+        public struct Result
+        {
+            public int PixelsToInpaintAmount { get; set; }
+            public int PixelsChangedAmount { get; set; }
+            public double TotalDifference { get; set; }
+            public double ChangedPixelsDifference { get; set; }
+
+            public double ChangedPixelsPercent => (double)PixelsChangedAmount / (double)PixelsToInpaintAmount;
         }
     }
 }
