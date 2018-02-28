332e3292dd378bf4fc467af0b19c235e79c9bb80
zavolokas <sergey.zavoloka@gmail.com>
2018-02-28 20:59:23 +0100

    Test that image is passed before build

---
+++ Inpainting.UnitTests/GivenPyramidBuilder/WhenBuild.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Zavolokas.ImageProcessing.Inpainting.UnitTests.GivenPyramidBuilder
+{
+    [TestFixture]
+    public class WhenBuild
+    {
+        [Test]
+        public void Should_Throw_ImageNotProvidedException_When_No_Image_Was_Added()
+        {
+            // Arrange
+            var pyramidBuilder = new PyramidBuilder();
+            Action act = () => pyramidBuilder.Build();
+
+            // Act & Assert
+            act.ShouldThrow<ImageNotProvidedException>();
+        }
+    }
+}
