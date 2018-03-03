745d4f98a2e4d9fbcd46884a76e0d8804866a85c
zavolokas <sergey.zavoloka@gmail.com>
2018-03-03 08:04:55 +0100

    make sure the images are passed in ARGB format

--- Inpainting.UnitTests/GivenPyramidBuilder/WhenInit.cs
+++ Inpainting.UnitTests/GivenPyramidBuilder/WhenInit.cs
@@ -18,8 +18,8 @@ namespace Zavolokas.ImageProcessing.Inpainting.UnitTests.GivenPyramidBuilder
             // Arrange
             var markupImage = CreateImage(128, 128);
             var pyramidBuilder = new PyramidBuilder();
-            pyramidBuilder.Init(null, markupImage);
-            Action act = () => pyramidBuilder.Build(2);
+            
+            Action act = () => pyramidBuilder.Init(null, markupImage); 
 
             // Act & Assert
             act.ShouldThrow<ArgumentNullException>();
@@ -31,17 +31,64 @@ namespace Zavolokas.ImageProcessing.Inpainting.UnitTests.GivenPyramidBuilder
             // Arrange
             var image = CreateImage(128, 128);
             var pyramidBuilder = new PyramidBuilder();
-            pyramidBuilder.Init(image, null);
-            Action act = () => pyramidBuilder.Build(2);
+            Action act = () => pyramidBuilder.Init(image, null);
 
             // Act & Assert
             act.ShouldThrow<ArgumentNullException>();
         }
 
-        public ZsImage CreateImage(int width, int height, double val = 1.0)
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(6)]
+        public void Should_Throw_WrongImageFormatException_When_Not_Argb_Image(byte componenetsAmount)
         {
-            var pixels = Enumerable.Repeat(val, width * height * 4).ToArray();
-            return new ZsImage(pixels, width, height, 4);
+            // Arrange
+            var image = CreateImage(128, 128, componenetsAmount);
+            var markupImage = CreateImage(128, 128, 4);
+            var pyramidBuilder = new PyramidBuilder();
+            Action act = () => pyramidBuilder.Init(image, markupImage);
+
+            // Act & Assert
+            act.ShouldThrow<WrongImageFormatException>();
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(6)]
+        public void Should_Throw_WrongImageFormatException_When_Not_Argb_Markup(byte componenetsAmount)
+        {
+            // Arrange
+            var image = CreateImage(128, 128, 4);
+            var markupImage = CreateImage(128, 128, componenetsAmount); 
+            var pyramidBuilder = new PyramidBuilder();
+            Action act = () => pyramidBuilder.Init(image, markupImage);
+
+            // Act & Assert
+            act.ShouldThrow<WrongImageFormatException>();
+        }
+
+        [Test]
+        public void Should_Not_Throw_When_Images_Correct()
+        {
+            // Arrange
+            var image = CreateImage(128, 128);
+            var markupImage = CreateImage(128, 128);
+            var pyramidBuilder = new PyramidBuilder();
+            Action act = () => pyramidBuilder.Init(image, markupImage);
+
+            // Act & Assert
+            act.ShouldNotThrow();
+        }
+
+        public ZsImage CreateImage(int width, int height, byte componentsAmount = 4, double val = 1.0)
+        {
+            var pixels = Enumerable.Repeat(val, width * height * componentsAmount).ToArray();
+            return new ZsImage(pixels, width, height, componentsAmount);
         }
     }
 }
+
