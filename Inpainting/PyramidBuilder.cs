3fdec5da5cd20d25206a7af11218477a021ee8f3
zavolokas <sergey.zavoloka@gmail.com>
2018-03-03 14:08:13 +0100

    pyramid build implementation

--- Inpainting/PyramidBuilder.cs
+++ Inpainting/PyramidBuilder.cs
@@ -1,12 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Zavolokas.Structures;
 
 namespace Zavolokas.ImageProcessing.Inpainting
 {
     public class PyramidBuilder
     {
-        public void Init(ZsImage image, ZsImage markup)
+        private ZsImage _imageArgb;
+        private ZsImage _inpaintMarkup;
+        private readonly double[] _backgroundColor;
+
+        public PyramidBuilder()
         {
+            _backgroundColor = new[] { 0.0, 0.0, 0.0, 0.0 };
+        }
+
+        public void Init(ZsImage imageArgb, ZsImage inpaintMarkupArgb)
+        {
+            if (imageArgb == null)
+                throw new ArgumentNullException(nameof(imageArgb));
+
+            if (inpaintMarkupArgb == null)
+                throw new ArgumentNullException(nameof(inpaintMarkupArgb));
+
+            if (imageArgb.NumberOfComponents != 4)
+                throw new WrongImageFormatException();
+
+            if (inpaintMarkupArgb.NumberOfComponents != 4)
+                throw new WrongImageFormatException();
+
+            _imageArgb = imageArgb;
+            _inpaintMarkup = inpaintMarkupArgb;
         }
 
         public void AddDonorMarkup(ZsImage donor)
@@ -14,13 +39,108 @@ namespace Zavolokas.ImageProcessing.Inpainting
         }
 
         /// <summary>
-        /// Builds an image pyramid with the specified levels amount.
+        /// Builds an imageArgb pyramid with the specified levels amount.
         /// </summary>
         /// <param name="levelsAmount">The levels amount.</param>
         /// <returns></returns>
-        public Pyramid Build(byte levelsAmount)
+        public Pyramid Build(byte levelsAmount, byte patchSize = 11)
         {
-            throw new NotImplementedException();
+            if (levelsAmount < 1)
+                throw new ArgumentOutOfRangeException(nameof(levelsAmount));
+
+            if (_imageArgb == null)
+                throw new InitializationException();
+
+            int w = _imageArgb.Width;
+            int h = _imageArgb.Height;
+
+            if (w == 1 || h == 1)
+                throw new WrongImageSizeException();
+
+            for (int levelInadex = 1; levelInadex < levelsAmount; levelInadex++)
+            {
+                if (w % 2 > 0 || h % 2 > 0)
+                    throw new WrongImageSizeException();
+
+                w /= 2;
+                h /= 2;
+
+                if (w == 1 || h == 1)
+                    throw new WrongImageSizeException();
+            }
+
+            var imageArea = Area2D.Create(0, 0, _imageArgb.Width, _imageArgb.Height);
+
+            if (_inpaintMarkup.Width != _imageArgb.Width || _inpaintMarkup.Height != _imageArgb.Height)
+            {
+                // Adjus inpaint markup image so that it become the same size as
+                // the image is
+                var markupArea = Area2D.Create(0, 0, _inpaintMarkup.Width, _inpaintMarkup.Height);
+                var srcArea = markupArea.Intersect(imageArea);
+                var pixels = Enumerable.Repeat(0.0, _imageArgb.Width * _imageArgb.Height * 4).ToArray();
+                var newInpaintMarkup = new ZsImage(pixels, _imageArgb.Width, _imageArgb.Height, 4);
+                _inpaintMarkup = newInpaintMarkup.CopyFromImage(imageArea, _inpaintMarkup, srcArea);
+            }
+
+            var inpaintArea = _inpaintMarkup.FromArgbToArea2D();
+
+            if (inpaintArea.IsEmpty)
+                throw new NoAreaToInpaintException();
+
+            // Build pyramids
+
+            var images = new List<ZsImage>(levelsAmount);
+            var mappings = new List<Area2DMap>(levelsAmount);
+            var markups = new List<Area2D>(levelsAmount);
+
+            var mapBuilder = new InpaintMapBuilder(new Area2DMapBuilder());
+
+            for (byte levelIndex = 0; levelIndex < levelsAmount; levelIndex++)
+            {
+                // convert image to Lab color space and store it
+                var imageCopy = _imageArgb.Clone()
+                    .FromArgbToRgb(_backgroundColor)
+                    .FromRgbToLab();
+                images.Add(imageCopy);
+
+                inpaintArea = _inpaintMarkup.FromArgbToArea2D();
+                imageArea = Area2D.Create(0, 0, _imageArgb.Width, _imageArgb.Height);
+                var nnfSourceArea = imageArea.Substract(inpaintArea);
+                Area2D nnfTargetArea;
+
+                // Obtain target area for the NNF building based on the level
+                if (levelIndex == levelsAmount - 1)
+                {
+                    // For the top level, the target area should be the whole image area
+                    nnfTargetArea = imageArea;
+                }
+                else
+                {
+                    // For the rest levels, the target area should be
+                    // slightly bigger then the inpaint area.
+                    nnfTargetArea = inpaintArea
+                        .Dilation(patchSize)
+                        .Intersect(imageArea);
+                }
+
+                // Create a mapping for the level.
+                var mapping = mapBuilder.InitNewMap(nnfTargetArea)
+                    .SetInpaintArea(inpaintArea)
+                    .Build();
+
+                mappings.Add(mapping);
+                markups.Add(inpaintArea);
+
+                if (levelIndex < levelsAmount - 1)
+                {
+                    // downscale for the next level
+                    // NOTE: we shouldn't blur out the inpainted area so it is not getting bigger!!
+                    _imageArgb.PyramidDownArgb(nnfSourceArea);
+                    _inpaintMarkup.PyramidDownArgb(false);
+                }
+            }
+
+            return new Pyramid(images, markups, mappings);
         }
     }
 }
