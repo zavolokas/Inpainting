51e8ef2b19f1b8645f758bef5af344d45b9f9871
zavolokas <sergey.zavoloka@gmail.com>
2018-03-05 07:22:26 +0100

    introduce LevelsDetector

---
+++ Inpainting/LevelsDetector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zavolokas.Structures;
+
+namespace Zavolokas.ImageProcessing.Inpainting
+{
+    public class LevelsDetector
+    {
+        public byte CalculateLevelsAmount(ZsImage image, ZsImage removeMarkup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
