a83608a80c5a19a1f331fd0e7f6317548194cb90
zavolokas <sergey.zavoloka@gmail.com>
2018-04-19 15:07:17 +0200

    init setting to avoid null ref exception

--- Samples/InpaintService/InpaintRequest.cs
+++ Samples/InpaintService/InpaintRequest.cs
@@ -5,6 +5,6 @@
         public string Container { get; set; }
         public string Image { get; set; }
         public string RemoveMask { get; set; }
-        public InpaintServiceSettings Settings { get; set; }
+        public InpaintServiceSettings Settings { get; set; } = new InpaintServiceSettings();
     }
 }
