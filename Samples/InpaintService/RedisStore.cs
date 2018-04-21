3ff2cddbb8fc04d34a2db84a35781b3bcd8e221a
zavolokas <sergey.zavoloka@gmail.com>
2018-04-21 07:05:36 +0200

    added Redis store

---
+++ Samples/InpaintService/RedisStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Azure.WebJobs.Host;
+using StackExchange.Redis;
+
+namespace InpaintService
+{
+    public class RedisStore
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection;
+
+        static RedisStore()
+        {
+            var connectionString = AmbientConnectionStringProvider.Instance.GetConnectionString("Redis");
+
+            LazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connectionString));
+        }
+
+
+        //var redis = RedisStore.RedisCache;
+
+        //    if(redis.StringSet("testKey", "testValue"))
+        //{
+        //    var val = redis.StringGet("testKey");
+
+        //    Console.WriteLine(val);
+        //}
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        public static IDatabase RedisCache => Connection.GetDatabase();
+    }
+}
