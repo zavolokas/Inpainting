0d8de2239125fb901d0c6aeceadfae054bd25fb3
ferib <lopke123@live.nl>
2019-10-03 18:45:46 +0200

    added Nancy & test API

---
+++ InpaintAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace InpaintAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Server...");
+            WebHost webHost = new WebHost();
+
+            Thread.Sleep(-1);
+        }
+    }
+}
