ee8422986cab51c777fb8cb040a3fc09d884a9c3
ferib <lopke123@live.nl>
2019-10-04 03:24:02 +0200

    change framework

---
+++ Samples/InpaintHTTP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace InpaintHTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating new server..");
+            WebHost webHost = new WebHost();
+            Thread.Sleep(-1);
+        }
+    }
+}
