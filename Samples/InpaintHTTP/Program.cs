ea9a8efd68d6da5be650239076608af7308b4816
Ferib Hellscream <lopke123@live.nl>
2019-10-05 19:26:08 +0200

    sample REST API (#24)
    Added a web based sample code

---
+++ Samples/InpaintHTTP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace InpaintHTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating new server..");
+            NancyHost nancyHost = new NancyHost(new Uri("http://localhost:8069"));
+            nancyHost.Start();
+            Console.WriteLine("Initialized and started NancyHost");
+
+            //inf sleep to avoid main thread from finishing/terminating
+            Thread.Sleep(-1); 
+        }
+    }
+}
