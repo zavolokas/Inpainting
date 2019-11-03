271e2f449c857ced779e336d96b0495c337e9561
zavolokas <sergey.zavoloka@gmail.com>
2019-11-03 09:35:01 +0000

    Accord doesn't support .NET Standard

--- Inpainting/ColorResolvers/MeanShiftColorResolver.cs
+++ Inpainting/ColorResolvers/MeanShiftColorResolver.cs
@@ -1,9 +1,13 @@
-﻿using System.Linq;
-using Accord.MachineLearning;
-using Accord.Statistics.Distributions.DensityKernels;
+﻿using System;
+using System.Linq;
+
+// Accord doesn't have a support for .NET Standard
+//using Accord.MachineLearning;
+//using Accord.Statistics.Distributions.DensityKernels;
 
 namespace Zavolokas.ImageProcessing.Inpainting
 {
+    [Obsolete("Use SimpleColorResolver", true)]
     public class MeanShiftColorResolver : ColorResolver
     {
         private readonly ColorResolver _colorResolver;
@@ -15,54 +19,55 @@ namespace Zavolokas.ImageProcessing.Inpainting
         public override void Resolve(double[] weightedColors, int startIndex, int colorsAmount, byte componentsAmount, double k,
             double[] resultColor, int resultIndex)
         {
-            var els = componentsAmount + 1;
-            var offs = startIndex * els;
+            throw new NotImplementedException();
+            //var els = componentsAmount + 1;
+            //var offs = startIndex * els;
 
-            var colors = weightedColors
-                .Skip(offs)
-                .Take(colorsAmount * els)
-                .ToArray();
+            //var colors = weightedColors
+            //    .Skip(offs)
+            //    .Take(colorsAmount * els)
+            //    .ToArray();
 
-            var observations = new double[colorsAmount][];
-            for (int colorIndex = 0; colorIndex < colorsAmount; colorIndex++)
-            {
-                var weightedColor = new double[els];
-                observations[colorIndex] = weightedColor;
+            //var observations = new double[colorsAmount][];
+            //for (int colorIndex = 0; colorIndex < colorsAmount; colorIndex++)
+            //{
+            //    var weightedColor = new double[els];
+            //    observations[colorIndex] = weightedColor;
 
-                for (int i = 0; i < els; i++)
-                {
-                    weightedColor[i] = weightedColors[offs + colorIndex * els + i];
-                }
-            }
+            //    for (int i = 0; i < els; i++)
+            //    {
+            //        weightedColor[i] = weightedColors[offs + colorIndex * els + i];
+            //    }
+            //}
 
-            // Create a uniform kernel density function
-            UniformKernel kernel = new UniformKernel();
+            //// Create a uniform kernel density function
+            //UniformKernel kernel = new UniformKernel();
 
-            //calculate sigma
-            double sigma = CalculateSigma(colors, (byte)(componentsAmount + 1));
+            ////calculate sigma
+            //double sigma = CalculateSigma(colors, (byte)(componentsAmount + 1));
 
-            //calc bandwidth
-            double bandwidth = k * sigma;
+            ////calc bandwidth
+            //double bandwidth = k * sigma;
 
-            // Create a new Mean-Shift algorithm for 4 dimensional samples
-            MeanShift meanShift = new MeanShift(kernel: kernel, bandwidth: bandwidth);
+            //// Create a new Mean-Shift algorithm for 4 dimensional samples
+            //MeanShift meanShift = new MeanShift(kernel: kernel, bandwidth: bandwidth);
 
-            // Compute the algorithm, retrieving an integer array
-            //  containing the labels for each of the observations
-            int[] labels = meanShift.Compute(observations);
-            var mostCommonColor = labels
-                .GroupBy(item => item)
-                .OrderByDescending(gg => gg.Count())
-                .Select(gg => gg.Key)
-                .First();
+            //// Compute the algorithm, retrieving an integer array
+            ////  containing the labels for each of the observations
+            //int[] labels = meanShift.Compute(observations);
+            //var mostCommonColor = labels
+            //    .GroupBy(item => item)
+            //    .OrderByDescending(gg => gg.Count())
+            //    .Select(gg => gg.Key)
+            //    .First();
 
-            var labeledWeightedColors = labels
-                .Where(l => l == mostCommonColor)
-                .SelectMany((label, index) => observations[index])
-                .ToArray();
+            //var labeledWeightedColors = labels
+            //    .Where(l => l == mostCommonColor)
+            //    .SelectMany((label, index) => observations[index])
+            //    .ToArray();
 
 
-            _colorResolver.Resolve(labeledWeightedColors, 0, labeledWeightedColors.Length / els, componentsAmount, k, resultColor, resultIndex);
+            //_colorResolver.Resolve(labeledWeightedColors, 0, labeledWeightedColors.Length / els, componentsAmount, k, resultColor, resultIndex);
         }
 
         private static double CalculateSigma(double[] info, byte cmp)
